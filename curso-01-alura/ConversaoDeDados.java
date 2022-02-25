1014763d387c978917d52d84b40f6ffe2c6b6320
Rui Nascimento <sruy19@gmail.com>
2022-02-25 12:41:14 -0300

    Coversao de double para int

---
+++ curso-01-alura/ConversaoDeDados.java
@@ -0,0 +1,13 @@
+public class ConversaoDeDados {
+    public static void main(String[] args) {
+        double salario = 1800.07;
+        //conversãode double para int
+        int salarioInt = (int) salario;
+
+        System.out.println("Salario double: " + salario + "\nSalario int: " + salarioInt);
+
+        double numero1 = 0.1;
+        double numero2 = 0.2;
+        System.out.println(numero1 + numero2);
+    }    
+}
