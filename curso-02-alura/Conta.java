af24fedfb9551ef412d20f579453638a20089835
sruiNascimento <sruy19@gmail.com>
2022-03-09 22:56:56 -0300

    metodos deposita, sacar e transfere

--- curso-02-alura/Conta.java
+++ curso-02-alura/Conta.java
@@ -9,8 +9,6 @@ public class Conta {
         this.agencia = agencia;
         this.numero = numero;
         this.titular = titular;
-
-        System.out.println("Conta criada!");
     }
 
     public String getToString() {
@@ -20,5 +18,31 @@ public class Conta {
                 "Saldo RS: " + this.saldo + "\n";
     }
 
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean depositar(double valorDepositado) {
+        if (valorDepositado <= 0)   return false;
+
+        this.saldo += valorDepositado;
+        return true;
+    }
+
+    public boolean sacar(double valorASerSacado) {
+        if (!this.temSaldo(valorASerSacado)) return false;
+        this.saldo -= valorASerSacado;
+        return true;
+    }
+
+    private boolean temSaldo(double valor) {
+        return valor <=  this.saldo;
+    }
+
+    public boolean transferir(double valorASerTransferido, Conta contaDestino) {
+        if(!this.sacar(valorASerTransferido)) return false;     
+        contaDestino.depositar(valorASerTransferido);
+        return true;
+    }
 
 }
--- curso-02-alura/MainConta.java
+++ curso-02-alura/MainConta.java
@@ -1,6 +1,24 @@
 public class MainConta {
     public static void main(String[] args) {
         Conta conta1 = new Conta(123, 5551, "Rui");
+        //Conta conta2 = conta1;
+        conta1.setSaldo(1500);
+        //System.out.println(conta1.getToString());
+        //System.out.println(conta2.getToString());
+
+        Conta conta2 = new Conta(124, 5552, "Nara");
+        //conta1.depositar(124);
+        //String resultadoDoDeposito =  conta2.depositar(124)? "Deposito realizado com sucesso": "Falha ao realizar deposito" ;
+        //System.out.println(resultadoDoDeposito);
+        //System.out.println(conta2.getToString());
+
+        System.out.println(conta1.sacar(100));
+
+        conta1.transferir(1000, conta2);
+        //conta1.transferir(1500, conta2);
+        
+
         System.out.println(conta1.getToString());
+        System.out.println(conta2.getToString());
     }
 }
